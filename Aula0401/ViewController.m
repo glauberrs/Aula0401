e75a1d2868981d9bff17c6d63f0a6c7e98ebeba8
Glauber Rodrigues da Silva <glauberrs@gmail.com>
2015-05-22 12:05:01 -0300

    Commit Inicial

--- Aula0401/ViewController.m
+++ Aula0401/ViewController.m
@@ -16,12 +16,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    int x = 0;
+    int y = 0;
+    int cx = 0;
+    for (int i = 1; i <=60; i++) {
+        if (i % 4 == 1) {
+            x = 10;
+            y += 90;
+        } else {
+            x += 90;
+        }
+        CGRect labelFrame = CGRectMake(x, y, 80, 80 );
+        UILabel* label = [[UILabel alloc] initWithFrame: labelFrame];
+        [label setText: [NSString stringWithFormat:@"%d", i]];
+        [label setBackgroundColor: [UIColor orangeColor]];
+        [self.scrollView addSubview: label];
+        cx += label.frame.size.height + 1;
+    }
+    [self.scrollView setContentSize:CGSizeMake([self.scrollView bounds].size.width , cx)];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
